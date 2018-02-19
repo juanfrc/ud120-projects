ec8f4a6792dddf17b8ec058a9a8465707f3ef081
juanfrc <juanfrc2@gmail.com>
2018-02-18 22:58:30 -0500

    add SVM logic for chapter 3 mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -20,11 +20,30 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+from sklearn import svm
+clf = svm.SVC(C=10000)
+# clf = svm.SVC(kernel='linear')
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 =time()
+score = clf.score(features_test, labels_test)
+print "prediction time:", round(time()-t1, 3), "s"
+print score
+
+# print pred[10]
+# print pred[26]
+# print pred[50]
+
+print sum(pred)
 
 
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+# c=10, accuracy = 0.616040955631
+# c=100, accuracy = 0.616040955631
+# c=1000, accuracy = 0.821387940842
+# c=10000, accuracy = 0.892491467577
